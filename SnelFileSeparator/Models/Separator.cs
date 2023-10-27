fde693151d1778bfc9906f3c37a4baf5bf34444e
Andrii Litvinov <andreoyy.litvinovv@gmail.com>
2023-10-27 18:33:31 +0300

    idk

---
+++ SnelFileSeparator/Models/Separator.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SnelFileSeparator.Models
+{
+    internal static class Separator
+    {
+        public static List<Record> AzimuthMinMax(double min, double max, List<Record> records)
+        {
+            var edited = from r in records
+                         where r.AzimuthAngle >= min && r.AzimuthAngle <= max
+                         select r;
+
+            return edited.ToList();
+        }
+
+        public static List<Record> AzimuthFrom(double min, List<Record> records)
+        {
+            var edited = from r in records
+                         where r.AzimuthAngle >= min
+                         select r;
+
+            return edited.ToList();
+        }
+
+        public static List<Record> AzimuthTo(double max, List<Record> records)
+        {
+            var edited = from r in records
+                         where r.AzimuthAngle <= max
+                         select r;
+
+            return edited.ToList();
+        }
+
+        public static List<Record> VizirMinMax(double min, double max, List<Record> records)
+        {
+            var edited = from r in records
+                         where r.ViewingAngle >= min && r.ViewingAngle <= max
+                         select r;
+
+            return edited.ToList();
+        }
+
+        public static List<Record> VizirFrom(double min, List<Record> records)
+        {
+            var edited = from r in records
+                         where r.ViewingAngle >= min
+                         select r;
+
+            return edited.ToList();
+        }
+
+        public static List<Record> VizirTo(double max, List<Record> records)
+        {
+            var edited = from r in records
+                         where r.ViewingAngle <= max
+                         select r;
+
+            return edited.ToList();
+        }
+    }
+}
